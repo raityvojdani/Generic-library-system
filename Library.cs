f61812c10488ba9d9ca202400405ce16f187e921
raity <82751237+raityvojdani@users.noreply.github.com>
2024-09-12 16:45:20 +0330

    Update Library.cs

--- Library.cs
+++ Library.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+    /// <summary>
+    /// Represents a library that stores various items.
+    /// </summary>
     public class Library
     {
+        // List to store library items
         private List<object> _items = new List<object>();
 
-        // Generic method to add items to the library
+        /// <summary>
+        /// Adds a new item to the library.
+        /// </summary>
+        /// <typeparam name="T">The type of the item being added.</typeparam>
+        /// <param name="item">The library item to add.</param>
         public void AddItem<T>(LibraryItem<T> item)
         {
             _items.Add(item);
         }
 
+        /// <summary>
+        /// Retrieves all the items stored in the library.
+        /// </summary>
+        /// <returns>An enumerable collection of all library items.</returns>
         public IEnumerable<object> GetAllItems()
         {
             return _items;
