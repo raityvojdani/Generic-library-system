5d7e8a8eddf0486bc595e9483b0cef4f362684b8
raity <82751237+raityvojdani@users.noreply.github.com>
2024-09-12 16:44:43 +0330

    Update libraryItem.cs

--- libraryItem.cs
+++ libraryItem.cs
@@ -1,14 +1,28 @@
-﻿namespace ConsoleApp1
+namespace ConsoleApp1
 {
+    /// <summary>
+    /// Represents a generic library item.
+    /// </summary>
+    /// <typeparam name="T">The type of the library item.</typeparam>
     public class LibraryItem<T>
     {
+        /// <summary>
+        /// Gets the item stored in the library.
+        /// </summary>
         public T Item { get; private set; }
 
+        /// <summary>
+        /// The item to store in the library
+        /// </summary>
         public LibraryItem(T item)
         {
             Item = item;
         }
 
+        /// <summary>
+        /// Returns a string that represents the current library item.
+        /// </summary>
+     
         public override string ToString()
         {
             return Item.ToString();
